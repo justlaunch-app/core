1d2aac05b81cc069120c75c873fd96b15c6af467
Zoltan Fodor <me@zoltanfodor.dev>
2024-07-17 18:52:07 +0200

    CHORE: add token-cache

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Stack } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
@@ -37,6 +38,31 @@ export {
 
 export const unstable_settings = {
   initialRouteName: 'index',
+};
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log('No values stored under key: ' + key);
+      }
+      return item;
+    } catch (error) {
+      console.error('SecureStore get item error: ', error);
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
 };
 
 const clerkApiKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY || '';
@@ -84,7 +110,7 @@ function RootLayoutNav() {
         key={`root-status-bar-${colorScheme === 'dark' ? 'light' : 'dark'}`}
       />
 
-      <ClerkProvider publishableKey={clerkApiKey}>
+      <ClerkProvider publishableKey={clerkApiKey} tokenCache={tokenCache}>
         <ThemeProvider value={NAV_THEME[colorScheme]}>
           <GestureHandlerRootView style={{ flex: 1 }}>
             <ClerkLoaded>
